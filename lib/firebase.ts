ba0fb50128b1f14163e3024e93d552c97c0a31e8
garidenkhbat9-blip <garidenkhbat9@gmail.com>
2026-05-15 13:03:18 +0800

    update firebase config with env vars

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAWD6xtSfVnxbPePu423KpKJm4OqqH6FR8",
-  authDomain: "unurflower-0913.firebaseapp.com",
-  projectId: "unurflower-0913",
-  storageBucket: "unurflower-0913.firebasestorage.app", 
-  messagingSenderId: "184666178926",
-  appId: "1:184666178926:web:cf90b53545307e9fef6d7f"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "unurflower-0913.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "unurflower-0913",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "unurflower-0913.firebasestorage.app", 
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "184666178926",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:184666178926:web:cf90b53545307e9fef6d7f"
 };
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
